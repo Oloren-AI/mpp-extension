93bc26cfd600349ac8fb129c01236d577f8ebea6
Raunak Chowdhuri <raunak@oloren.ai>
2023-04-13 20:53:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Extension Sample
+
+This is a sample extension which implements each of the basic extension components in a simple/extensible framework.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LocalButton from "./Button";
+
+const App = () => (
+  <div>
+    <h1>Typescript</h1>
+    <h2>App 3</h2>
+  </div>
+);
+
+export default App;
