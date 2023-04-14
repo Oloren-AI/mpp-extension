e414d31618c8ea8bc7992a628296f7ac102841d4
Raunak Chowdhuri <raunak@oloren.ai>
2023-04-14 12:50:32 -0400

    fix typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
 import React from "react";
 import NumberNode from "./nodes/NumberNode";
-import type { FlowNode, NodeSetter } from "./util";
+import type { FlowNodeData, NodeSetter } from "./util";
+import { Typography } from "antd";
 import OperationNode from "./nodes/OperationNode";
+
+const { Title } = Typography;
 
 function NodeTester({
   Node,
 }: {
-  Node: React.FC<{ node: FlowNode; setNode: NodeSetter }>;
+  Node: React.FC<{ node: FlowNodeData; setNode: NodeSetter }>;
 }) {
-  const [node, setNode] = React.useState<FlowNode>({
-    id: "node-0",
-    data: {
-      data: {},
-      operator: "",
-      hierarchy: [],
-      num_inputs: 1,
-      num_outputs: 1,
-    },
-    position: {
-      x: 200,
-      y: 100,
-    },
-    type: "FlowNode",
+  const [node, setNode] = React.useState<FlowNodeData>({
+    data: {},
+    operator: "",
+    hierarchy: [],
+    num_inputs: 1,
+    num_outputs: 1,
   });
 
   return (
     <div tw="flex flex-col mx-auto w-full max-w-2xl">
-      <h1>{Node.name}</h1>
+      <Title>{Node.name}</Title>
       <div tw="p-2 bg-white border-solid border border-gray-300 w-fit">
         <Node node={node} setNode={setNode} />
       </div>
